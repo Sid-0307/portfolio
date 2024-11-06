56b081ba6ab7f6ba93df584b7f543375cada2f40
Sid-0307 <siddharthshankar03@gmail.com>
2024-11-06 13:24:47 +0530

    Landing page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,31 +5,53 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
   const [showPhoto, setShowPhoto] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => setShowPhoto(true), 2500);
-    return () => clearTimeout(timer);
+    const handleScroll = () => {
+      if (window.scrollY > 5) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <div className="portfolio-container">
-      <div>
-        {showPhoto ? (
-          <div className="fullscreen-text">
-            <p className="fullstack">FULL STACK</p>
-            <div className="developer">
-              <div className="devel">DEVEL</div>
-              <div>
-                <img src={sid} className="photo" alt="Sid" />
+      {showPhoto ? (
+        <div className={`content ${scrolled ? "scrolled" : ""}`}>
+          {!scrolled ? (
+            <>
+              <img src={sid} className="photo" alt="Sid" />
+              <h1 className="greeting gradient">SIDDHARTH</h1>
+            </>
+          ) : (
+            <>
+              <div className="fullscreen-text">
+                <p className="fullstack gradient">FULL STACK</p>
+                <div className="developer">
+                  <div className="devel gradient">DEVEL</div>
+                  <div>
+                    <img src={sid} className="photo-small" alt="Sid" />
+                  </div>
+                  <div className="gradient">PER</div>
+                </div>
               </div>
-              <div>PER</div>
-            </div>
-          </div>
-        ) : (
-          // <img src={sid} className="photo" alt="Sid" onClick={handleClick} />
-          <img src={puff} className="loader" alt="Puff Loader" />
-        )}
-      </div>
+            </>
+          )}
+        </div>
+      ) : (
+        <img src={puff} className="loader" alt="Puff Loader" />
+      )}
     </div>
   );
 };
