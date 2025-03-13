8f393df1ea77fc0872900511c2fcba198fdb4277
Sid-0307 <siddharthshankar03@gmail.com>
2025-03-13 13:41:20 +0530

    Added trivia game to projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,6 +8,7 @@ import movie from "../assets/movie.png";
 import dockify from "../assets/dockify.png";
 import access from "../assets/access.png";
 import vizalpay from "../assets/vizalpay.png";
+import triviaGame from "../assets/trivia-game.png";
 import dqr from "../assets/dqr.png";
 
 const Project = forwardRef((props, ref) => {
@@ -22,12 +23,23 @@ const Project = forwardRef((props, ref) => {
   };
 
   const [activeIndex, setActiveIndex] = useState(0);
-  const projects = [stock, dockify, dqr, movie, rems, vizalpay, access, snake];
+  const projects = [
+    stock,
+    dockify,
+    dqr,
+    movie,
+    triviaGame,
+    rems,
+    vizalpay,
+    access,
+    snake,
+  ];
   const projectNames = [
     "Stock Portfolio Management",
     "Dockify",
     "Digital QR",
     "Movies4U",
+    "Trivia Game",
     "Real Estate Management",
     "VizalPay",
     "Access Management System",
@@ -39,6 +51,7 @@ const Project = forwardRef((props, ref) => {
     "Dockify is an AI-powered documentation tool that generates markdown files from a repository URL or zip file. It streamlines the process of creating technical documentation, ensuring clear, organized, and developer-friendly outputs for any project.",
     "A dynamic QR code generator that allows users to upload images and embed location tags. The tool creates scannable QR codes for quick access, making it ideal for sharing location-based content.",
     "A movie browsing platform where users can explore films or search them by genre or text. It offers a smooth interface for discovering movie details and allows easy navigation through a wide variety of films.",
+    "A multiplayer trivia game where players can create or join rooms to compete in real-time. The game features dynamic question sets, real-time score updates, and player tracking, ensuring a seamless and interactive experience.",
     "A web-based platform for listing, buying, and selling properties. Users can explore detailed property listings and manage their own, offering a simple and efficient way to navigate the real estate market.",
     "A payment app prototype designed with dummy cash to simulate digital transactions. Users can send and receive funds within the app and view detailed transaction histories of the transactions.",
     "An access management system designed for user authentication and role-based access control. It enables secure logins, efficient permission management in web applications.",
@@ -50,6 +63,9 @@ const Project = forwardRef((props, ref) => {
     "https://skillicons.dev/icons?i=html,css,javascript,python,fastapi,ai",
     "https://skillicons.dev/icons?i=figma,java,androidstudio,gradle,firebase",
     "https://skillicons.dev/icons?i=react,css,javascript,nodejs,express,mongodb",
+
+    "https://skillicons.dev/icons?i=flutter,dart,androidstudio,gradle,firebase",
+
     "https://skillicons.dev/icons?i=html,css,javascript,java,jquery,mysql",
     "https://skillicons.dev/icons?i=figma,java,androidstudio,gradle,sqlite",
     "https://skillicons.dev/icons?i=html,css,javascript,nodejs,mongodb",
@@ -61,6 +77,7 @@ const Project = forwardRef((props, ref) => {
     "https://github.com/Sid-0307/Dockify",
     "https://github.com/Sid-0307/DigitalQR",
     "https://github.com/Sid-0307/MovieBrowser",
+    "https://github.com/Sid-0307/TriviaApp",
     "https://github.com/Sid-0307/RealEstate-Management-System",
     "https://github.com/Sid-0307/VizalPay",
     "https://github.com/Sid-0307/Access-Management-System",
